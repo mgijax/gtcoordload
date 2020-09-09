dfcfe2a1e796d8481d51a1517a91d0e8518e0b20
Sharon Cousins Giannatto <sharon.giannatto@jax.org>
2020-09-09 15:24:56 -0400

    change permissions

--- bin/createInputFile.py
+++ bin/createInputFile.py
@@ -1,4 +1,3 @@
-#!/usr/local/bin/python
 
 #
 # Program: createInputFile.py
@@ -61,7 +60,7 @@ seqsToToggleList = []
 server = os.environ['MGD_DBSERVER']
 database = os.environ['MGD_DBNAME']
 user = os.environ['MGD_DBUSER']
-password = string.strip(open(os.environ['MGD_DBPASSWORDFILE'], 'r').readline())
+password = str.strip(open(os.environ['MGD_DBPASSWORDFILE'], 'r').readline())
 
 def loadLookup():
     global seqsToToggleList
@@ -72,26 +71,26 @@ def loadLookup():
 
     # get eucomm mutant cell lines (MCLs)
     cmds.append('''select c._CellLine_key
-	into #eucommCL
-	from  ALL_CellLine c, ALL_CellLine_Derivation d
-	where d._Creator_key = 4856374
-	and d._Derivation_key = c._Derivation_key''')
+        into temporary table eucommCL
+        from  ALL_CellLine c, ALL_CellLine_Derivation d
+        where d._Creator_key = 4856374
+        and d._Derivation_key = c._Derivation_key''')
 
-    cmds.append('create index idx_1 on #eucommCL(_CellLine_key)') 
+    cmds.append('create index idx_1 on eucommCL(_CellLine_key)') 
 
     # get sequences associated with  eucomm MCLs (via the allele)
     cmds.append('''select t._CellLine_key, saa._Sequence_key
-    into #eucommSeq
-    from #eucommCL t, ALL_Allele_CellLine aac, SEQ_Allele_Assoc saa
+    into temporary table eucommSeq
+    from eucommCL t, ALL_Allele_CellLine aac, SEQ_Allele_Assoc saa
     where t._CellLine_key = aac._MutantCellLine_key
     and aac._Allele_key = saa._Allele_key''')
 
-    cmds.append('create index idx_1 on #eucommSeq(_Sequence_key)')
+    cmds.append('create index idx_1 on eucommSeq(_Sequence_key)')
 
     # filter for just the upstream vector end and get the seqID
     cmds.append('''select t.*, a.accID
-    into #eucommUpstSeq
-    from #eucommSeq t, SEQ_GeneTrap sgt, ACC_Accession a
+    into temporary table eucommUpstSeq
+    from eucommSeq t, SEQ_GeneTrap sgt, ACC_Accession a
     where t._Sequence_key = sgt._Sequence_key
     and sgt._VectorEnd_key = 3983010
     and sgt._Sequence_key = a._Object_key
@@ -101,26 +100,26 @@ def loadLookup():
 
     # get tigem MCLs 
     cmds.append('''select c._CellLine_key
-    into #tigemCL
+    into temporary table tigemCL
     from  ALL_CellLine c, ALL_CellLine_Derivation d
     where d._Creator_key = 3982963
     and d._Derivation_key = c._Derivation_key''')
 
-    cmds.append('create index idx_1 on #tigemCL(_CellLine_key)')
+    cmds.append('create index idx_1 on tigemCL(_CellLine_key)')
 
     # get sequences associated with tigem MCLs (via allele)
     cmds.append('''select t._CellLine_key, saa._Sequence_key
-    into #tigemSeq
-    from #tigemCL t, ALL_Allele_CellLine aac, SEQ_Allele_Assoc saa
+    into temporary table tigemSeq
+    from tigemCL t, ALL_Allele_CellLine aac, SEQ_Allele_Assoc saa
     where t._CellLine_key = aac._MutantCellLine_key
     and aac._Allele_key = saa._Allele_key''')
 
-    cmds.append('create index idx_1 on #tigemSeq(_Sequence_key)')
+    cmds.append('create index idx_1 on tigemSeq(_Sequence_key)')
     
     # get the seqID
     cmds.append('''select t.*, a.accID
-    into #tigemAccid
-    from #tigemSeq t, ACC_Accession a
+    into temporary table tigemAccid
+    from tigemSeq t, ACC_Accession a
     where t._Sequence_key  = a._Object_key
     and a._MGIType_key = 19
     and a._LogicalDB_key = 9
@@ -128,28 +127,28 @@ def loadLookup():
 
     # union the eucomm and tigem sets
     cmds.append('''select t.accID
-    into #all
-    from #eucommUpstSeq t
+    into temporary table all
+    from eucommUpstSeq t
     union
     select te.accID
-    from #tigemAccid te''')
+    from tigemAccid te''')
 
     # load the lookup list
-    cmds.append('select distinct * from #all')
+    cmds.append('select distinct * from all')
     results = db.sql(cmds, 'auto')
     for r in results[11]:
-	seqID = r['accID']
-	seqsToToggleList.append(seqID)
+        seqID = r['accID']
+        seqsToToggleList.append(seqID)
 
 def init():
-	#
-	# requires: 
-	#
-	# effects: 
-	# 1. Initializes global file descriptors/file names
-	#
-	# returns:
-	#
+        #
+        # requires: 
+        #
+        # effects: 
+        # 1. Initializes global file descriptors/file names
+        #
+        # returns:
+        #
  
     global outFile, outFileName
  
@@ -162,29 +161,29 @@ def init():
 def run():
     line = sys.stdin.readline()
     while line != "":
-	tokenList =  string.splitfields(line, TAB)
-	if tokenList[0].find('scaf') != -1:
-	    line = sys.stdin.readline()
-	    continue
-	if tokenList[2] != 'gene':
-	    line = sys.stdin.readline()
-	    continue
-	chr = string.strip(tokenList[0])
-	# strip off the leading 'chr'
-	chr = chr[3:]
-	start = string.strip(tokenList[3])
-	end = string.strip(tokenList[4])
-	strand = string.strip(tokenList[6])
-	# strip of leading "Gene "
-	id = string.strip(tokenList[8][4:])
-	if id in seqsToToggleList:
-	    if strand == '+':
-		strand = '-'
-	    else:
-		strand = '+'
-	outFile.write("%s%s%s%s%s%s%s%s%s%s" % \
-	    (id, TAB, chr, TAB, start, TAB, end, TAB, strand, CRT))
-	line = sys.stdin.readline()
+        tokenList =  str.splitfields(line, TAB)
+        if tokenList[0].find('scaf') != -1:
+            line = sys.stdin.readline()
+            continue
+        if tokenList[2] != 'gene':
+            line = sys.stdin.readline()
+            continue
+        chr = str.strip(tokenList[0])
+        # strip off the leading 'chr'
+        chr = chr[3:]
+        start = str.strip(tokenList[3])
+        end = str.strip(tokenList[4])
+        strand = str.strip(tokenList[6])
+        # strip of leading "Gene "
+        id = str.strip(tokenList[8][4:])
+        if id in seqsToToggleList:
+            if strand == '+':
+                strand = '-'
+            else:
+                strand = '+'
+        outFile.write("%s%s%s%s%s%s%s%s%s%s" % \
+            (id, TAB, chr, TAB, start, TAB, end, TAB, strand, CRT))
+        line = sys.stdin.readline()
 
 
 #
